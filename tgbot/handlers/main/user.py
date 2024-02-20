204855e18e0f84f3a53dce81f8bb9858939eed90
Kostek <ksytnik73kwer@gmail.com>
2024-02-20 16:56:25 +0100

    Refactor user start message and add menu keyboard.

--- tgbot/handlers/main/user.py
+++ tgbot/handlers/main/user.py
@@ -2,9 +2,11 @@ from aiogram import Router
 from aiogram.filters import CommandStart
 from aiogram.types import Message
 
+from tgbot.keyboards.inline import menu_keyboard
+
 user_router = Router()
 
 
 @user_router.message(CommandStart())
 async def user_start(message: Message):
-    await message.reply("Вітаю, звичайний користувач!")
+    await message.reply(text="Вітаю, звичайний користувач!", reply_markup=menu_keyboard())
--- tgbot/keyboards/inline.py
+++ tgbot/keyboards/inline.py
@@ -1,73 +1,16 @@
-from aiogram.filters.callback_data import CallbackData
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 
 
-# This is a simple keyboard, that contains 2 buttons
-def very_simple_keyboard():
-    buttons = [
-        [
-            InlineKeyboardButton(text="📝 Створити замовлення",
-                                 callback_data="create_order"),
-            InlineKeyboardButton(text="📋 Мої замовлення", callback_data="my_orders"),
-        ],
-    ]
-
-    keyboard = InlineKeyboardMarkup(
-        inline_keyboard=buttons,
-    )
-    return keyboard
-
-
-# This is the same keyboard, but created with InlineKeyboardBuilder (preferred way)
-def simple_menu_keyboard():
-    # First, you should create an InlineKeyboardBuilder object
+def menu_keyboard():
     keyboard = InlineKeyboardBuilder()
 
-    # You can use keyboard.button() method to add buttons, then enter text and callback_data
+    keyboard.button(text="💳 Купить перевод", callback_data="buy_translate")
+    keyboard.button(text="⚠️ Сообщить об ошибке перевода", callback_data="report_error")
     keyboard.button(
-        text="📝 Створити замовлення",
-        callback_data="create_order"
-    )
-    keyboard.button(
-        text="📋 Мої замовлення",
-        # In this simple example, we use a string as callback_data
-        callback_data="my_orders"
+        text="💡 Написать пожелание / предложить свой перевод",
+        callback_data="offer_translate",
     )
 
-    # If needed you can use keyboard.adjust() method to change the number of buttons per row
-    # keyboard.adjust(2)
-
-    # Then you should always call keyboard.as_markup() method to get a valid InlineKeyboardMarkup object
-    return keyboard.as_markup()
-
-
-# For a more advanced usage of callback_data, you can use the CallbackData factory
-class OrderCallbackData(CallbackData, prefix="order"):
-    """
-    This class represents a CallbackData object for orders.
-
-    - When used in InlineKeyboardMarkup, you have to create an instance of this class, run .pack() method, and pass to callback_data parameter.
-
-    - When used in InlineKeyboardBuilder, you have to create an instance of this class and pass to callback_data parameter (without .pack() method).
-
-    - In handlers you have to import this class and use it as a filter for callback query handlers, and then unpack callback_data parameter to get the data.
-
-    # Example usage in simple_menu.py
-    """
-    order_id: int
-
-
-def my_orders_keyboard(orders: list):
-    # Here we use a list of orders as a parameter (from simple_menu.py)
-
-    keyboard = InlineKeyboardBuilder()
-    for order in orders:
-        keyboard.button(
-            text=f"📝 {order['title']}",
-            # Here we use an instance of OrderCallbackData class as callback_data parameter
-            # order id is the field in OrderCallbackData class, that we defined above
-            callback_data=OrderCallbackData(order_id=order["id"])
-        )
+    keyboard.adjust(1)
 
     return keyboard.as_markup()
