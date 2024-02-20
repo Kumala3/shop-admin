27534e82ba00253e8c254755ddbb5a21652bfd62
Kostek <ksytnik73kwer@gmail.com>
2024-02-20 11:08:24 +0100

    Refactor router imports and delete unnecessary simple_menu handler.

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,12 +1,10 @@
 """Import all routers and add them to routers_list."""
-from .admin import admin_router
-from .echo import echo_router
-from .simple_menu import menu_router
-from .user import user_router
+from .admin.admin import admin_router
+from .extra.echo import echo_router
+from .main.user import user_router
 
 routers_list = [
     admin_router,
-    menu_router,
     user_router,
     echo_router,  # echo_router must be last
 ]
