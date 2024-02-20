a947167511bff1dc0d2eaee7ded6a490ef05bf91
Kostek <ksytnik73kwer@gmail.com>
2024-02-20 19:22:05 +0100

    Delete unnecessary echo handler and remove echo_router from routers_list.

--- tgbot/handlers/__init__.py
+++ tgbot/handlers/__init__.py
@@ -1,12 +1,10 @@
 """Import all routers and add them to routers_list."""
 from .admin.admin import admin_router
-from .extra.echo import echo_router
 from .main.user import user_router
 
 routers_list = [
     admin_router,
     user_router,
-    echo_router,  # echo_router must be last
 ]
 
 __all__ = [
