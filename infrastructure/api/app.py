cec050cda620e3ce1bf73ccadb3b98cb4fb5ec0e
Kumala3 <ksytnik73kwer@gmail.com>
2024-02-25 01:12:35 +0300

    Update FastAPI webhook endpoint and admin start message

--- infrastructure/api/app.py
+++ infrastructure/api/app.py
@@ -1,17 +1,18 @@
 import logging
 
 import betterlogging as bl
-import fastapi
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from starlette.responses import JSONResponse
-
+from bot import bot
 
 app = FastAPI()
+    
 log_level = logging.INFO
 bl.basic_colorized_config(level=log_level)
 log = logging.getLogger(__name__)
 
 
 @app.post("/api")
-async def webhook_endpoint(request: fastapi.Request):
+async def webhook_endpoint(request: Request):
+    await bot.send_message(5850071804, "Hello from FastAPI")
     return JSONResponse(status_code=200, content={"status": "ok"})
