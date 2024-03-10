f4cd021af84c9e14ab4e1eab472d4fe722b6f6d1
Kumala3 <ksytnik73kwer@gmail.com>
2024-03-11 00:22:46 +0300

    Add payment_aaio endpoint for handling AAIO payments

--- infrastructure/api/app.py
+++ infrastructure/api/app.py
@@ -16,12 +16,14 @@ from infrastructure.admin_panel.authentication import AdminAuth
 from infrastructure.database.repo.requests import RequestsRepo
 from infrastructure.api.utils import get_repo
 from bot import bot
+from aiogram.types import URLInputFile
 
 config: Config = load_config(".env")
 engine = create_engine(config.db)
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 authentication_backend = AdminAuth(
     secret_key=config.admin_panel.secret_key,
@@ -56,7 +58,9 @@ async def webhook_endpoint(request: Request):
 @app.get("/action/enter_message", response_class=HTMLResponse)
 async def enter_message(request: Request):
     is_customers = request.query_params.get("customers")
-    return templates.TemplateResponse("message.html", {"request": request, "only_customers": is_customers})
+    return templates.TemplateResponse(
+        "message.html", {"request": request, "only_customers": is_customers}
+    )
 
 
 @app.post("/submit_message", response_class=HTMLResponse)
@@ -67,11 +71,11 @@ async def start_mailing(
 ):
     raw_text = await request.form()
     formatted_message = str(raw_text["message"].strip())
-    
+
     status = request.query_params.get("customers")
 
     customers_ids = await repo.purchases.get_customers_ids()
-    
+
     users_ids = await repo.users.get_users_ids()
 
     async def send_messages(message: str, users_ids: list):
@@ -101,5 +105,63 @@ async def start_mailing(
 @app.get("/test_user_ids")
 async def get_users_ids(repo: RequestsRepo = Depends(get_repo)):
     users_ids = await repo.purchases.get_customers_ids()
-    
+
     return {"users_ids": users_ids}
+
+
+@app.post("/payment_aaio")
+async def aaio_handler(request: Request, repo: RequestsRepo = Depends(get_repo)):
+    data = await request.form()
+    status = data["status"]
+
+    if status == "success":
+        photo = URLInputFile(
+            "https://drive.usercontent.google.com/download?id=1ZSx7ffkDGUBnBlme9I6ALVGYJxLLNH_1&export=download"
+        )
+        desc = data["desc"]
+        data = desc.split("|")
+
+        if "19.4" in data[1]:
+            file = URLInputFile(
+                "https://drive.usercontent.google.com/download?id=1n9-4qrc6t4scS19HIVb_QLQ6wLYeYsGH&export=download",
+                filename="SeoSpider_RU19.4.properties",
+            )
+            await bot.send_photo(
+                int(data[2]),
+                caption="Инструкция по установке перевода:\n1. Скачайте файл с переводом ниже;\n2. Закройте программу (если сейчас открыта);\n3. Измените название скачанного файла таким образом, чтобы он назывался «SeoSpider_ru»;\n4. Откройте папку «translations» где установлена программа, как правило дефолтные значения будут такие: C:\Program Files (x86)\Screaming Frog SEO Spider\ScreamingFrogSEOSpider\translations;\n5. Добавьте в архив скачанный файл;\n6. Запустите программу;",
+                photo=photo,
+            )
+            await bot.send_document((data[2]), file)
+        elif "19.2" in data[1]:
+            file = URLInputFile(
+                "https://drive.usercontent.google.com/download?id=1iH8s9g7a9HfbgpquOkpTDKrkPRJ40SQl&export=download",
+                filename="SeoSpider_RU19.2.properties",
+            )
+            await bot.send_photo(
+                int(data[2]),
+                caption="Инструкция по установке перевода:\n1. Скачайте файл с переводом ниже;\n2. Закройте программу (если сейчас открыта);\n3. Измените название скачанного файла таким образом, чтобы он назывался «SeoSpider_ru»;\n4. Откройте папку «translations» где установлена программа, как правило дефолтные значения будут такие: C:\Program Files (x86)\Screaming Frog SEO Spider\ScreamingFrogSEOSpider\translations;\n5. Добавьте в архив скачанный файл;\n6. Запустите программу;",
+                photo=photo,
+            )
+            await bot.send_document((data[2]), file)
+        elif "6.0" in data[1]:
+            file = URLInputFile(
+                "https://drive.usercontent.google.com/download?id=1ZX4_IKy1H9pyvJ8Ss9NAlAdZMEsswgvU&export=download",
+                filename="SeoSpider_EN6.0.properties",
+            )
+            await bot.send_photo(
+                int(data[2]),
+                caption="Инструкция по установке перевода:\n1. Скачайте файл с переводом ниже;\n2. Закройте программу (если сейчас открыта);\n3. Измените название скачанного файла таким образом, чтобы он назывался «LogFileAnalyser_en»;\n3. Откройте папку «translations» где установлена программа, как правило дефолтные значения будут такие: C:\Program Files (x86)\Screaming Frog Log File Analyser\ScreamingFrogLogFileAnalyser\translations;\n4. Сохраните где-нибудь исходный файл «LogFileAnalyser_en» на всякий случай;\n5. Замените текущий файл на скачанный;\n6. Запустите программу;",
+                photo=photo,
+            )
+            await bot.send_document((data[2]), file)
+        elif "5.3" in data[1]:
+            file = URLInputFile(
+                "https://drive.usercontent.google.com/download?id=1TJSAYj_NB9f6tU_cLn2vCKIxUfGlzI0M&export=download",
+                filename="SeoSpider_EN5.3.properties",
+            )
+            await bot.send_photo(
+                int(data[2]),
+                caption="Инструкция по установке перевода:\n1. Скачайте файл с переводом ниже;\n2. Закройте программу (если сейчас открыта);\n3. Измените название скачанного файла таким образом, чтобы он назывался «LogFileAnalyser_en»;\n3. Откройте папку «translations» где установлена программа, как правило дефолтные значения будут такие: C:\Program Files (x86)\Screaming Frog Log File Analyser\ScreamingFrogLogFileAnalyser\translations;\n4. Сохраните где-нибудь исходный файл «LogFileAnalyser_en» на всякий случай;\n5. Замените текущий файл на скачанный;\n6. Запустите программу;",
+                photo=photo,
+            )
+            await bot.send_document((data[2]), file)
